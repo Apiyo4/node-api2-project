ea4cc72bdd39c58fc5b8e2965ad87cdc2fc826f6
Annette Odingo <odingoannette@gmail.com>
2020-01-15 20:31:59 +0000

    Create an endpoint to get one post

--- index.js
+++ index.js
@@ -49,11 +49,27 @@ server.get('/api/posts', (req,res)=>{
     Posts.find()
     .then(posts=>{
         console.log(posts)
+        res.status(200).json(posts);
     })
     .catch(error=>{
         res.status(500).json({ error: "The posts information could not be retrieved." })
     })
 
+})
+server.get('/api/posts/:id', (req,res)=>{
+    const {id} = req.params;
+    if(id){
+        Posts.findById(id)
+        .then(post=>{
+            res.status(200).json(post);
+        })
+        .catch(error=>{
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+
+    }else{
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
 })
 server.listen(5000, ()=>{
     console.log('\n*** Server Running on http://localhost:5000 ***\n'); 
